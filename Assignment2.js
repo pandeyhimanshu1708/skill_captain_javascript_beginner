295ec46b83c386cebd20a07432dd92c12451487a
pandeyhimanshu1708 <97910793+pandeyhimanshu1708@users.noreply.github.com>
2023-11-13 22:48:52 +0530

    Update Assignment2.js

--- Assignment2.js
+++ Assignment2.js
@@ -1,14 +1,14 @@
 // Write a program that prints the first 10 multiples of a given number. If the number is negative or zero, print an error message.
 
 let number = 5;
-
 if (number <= 0) {
     console.log("error please enter a positive non-zero number");
 } else {
-    console.log(`First 10 multiples of ${number}:`);
+    console.log(`First 10 multiples of ${number} is:`);
     for (let i = 1; i <= 10; i++) {
         console.log(`${number} x ${i} = ${number * i}`);
+        
     }
 }
-Multiples(inputNumber);
+
   
