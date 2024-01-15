d57727811a950a7a1a8e1ae2e6467073d6151411
pandeyhimanshu1708 <97910793+pandeyhimanshu1708@users.noreply.github.com>
2024-01-15 22:40:43 +0530

    Add files via upload

---
+++ bubbleSort.js
@@ -0,0 +1,12 @@
+//Bubble Sort
+
+const bubbleSort = (array) => {
+    for (let i = array.length; i > 0; i--) {
+         for (let j = 0; j < i - 1; j++) {
+             if (array[j] > array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]];
+             }
+          }
+    }
+    return array;
+    };
