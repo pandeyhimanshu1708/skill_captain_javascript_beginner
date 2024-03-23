964222df03e14049402f0b29542731a0b9c3d9b5
pandeyhimanshu1708 <97910793+pandeyhimanshu1708@users.noreply.github.com>
2024-03-23 15:50:54 +0530

    Add files via upload

--- Day1.js
+++ Day1.js
@@ -1,7 +1,23 @@
-function replaceJavaScriptWithES6(text) {
-    const regex = /javascript/ig;
-    const modifiedText = text.replace(regex, `ES6`);
-    return modifiedText;
+class MyStack {
+    constructor() {
+        this.mainQueue = [];
+        this.tempQueue = [];
+    }
+    push(x) {
+        this.tempQueue.push(x);
+        while (this.mainQueue.length > 0) {
+            this.tempQueue.push(this.mainQueue.shift());
+        }
+        [this.mainQueue, this.tempQueue] = [this.tempQueue, this.mainQueue];
+    }
+    pop() {
+        return this.mainQueue.shift();
+    }
+    top() {
+        return this.mainQueue[0];
+    }
+    empty() {
+        return this.mainQueue.length === 0;
+    }
 }
-
 
