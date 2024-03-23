a01976e9df64239eed148d000a9375187dcb098d
pandeyhimanshu1708 <97910793+pandeyhimanshu1708@users.noreply.github.com>
2024-03-23 14:57:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+// Import
+const { factorial } = require('./utils');
+// define number
+const number = 5;
+const result = factorial(number);
+// result 
+console.log(`The factorial of ${number} is: ${result}`);
